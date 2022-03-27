0f7db3188f973dff42f6de5ae1d6146ccc89eab2
Kamran <iub.kamran@gmail.com>
2022-03-28 01:14:29 +0500

    Fixed bugs

--- conduct/settings.py
+++ conduct/settings.py
@@ -82,34 +82,30 @@ WSGI_APPLICATION = 'conduct.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-'''DATABASES = {
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hmsdb',
-<<<<<<< HEAD
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '54324'
-=======
         'USER': 'kamran',
         'PASSWORD': 'kamranpy',
         'HOST': 'db-1.ckraxyc8xpqt.us-west-2.rds.amazonaws.com',
         'PORT': '5432'
     }
-}'''
+}
+
+'''
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
->>>>>>> c07017611f64415e620d543692ef9086a83760e4
     }
 }
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
