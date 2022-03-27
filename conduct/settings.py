b6ae6657518a1167fa85cece9e6105d03fa1c540
Kamran <iub.kamran@gmail.com>
2022-03-28 01:59:05 +0500

    DB Changes

--- conduct/settings.py
+++ conduct/settings.py
@@ -102,9 +102,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -155,10 +155,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 #S3 BUCKETS CONFIG
 
-AWS_ACCESS_KEY_ID = 'AKIAWSIJ3BETTKDJ64R2'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'kamran-hms-bucket'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#AWS_ACCESS_KEY_ID = 'AKIAWSIJ3BETTKDJ64R2'
+#AWS_SECRET_ACCESS_KEY = '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'
+#AWS_STORAGE_BUCKET_NAME = 'kamran-hms-bucket'
+#AWS_S3_FILE_OVERWRITE = False
+#AWS_DEFAULT_ACL = None
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
